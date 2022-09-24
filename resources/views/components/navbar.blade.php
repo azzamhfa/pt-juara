414be84c4b295485e0768f7885cc42a13a3981f9
fathin210 <fathin210@gmail.com>
2022-09-24 12:02:37 +0700

    Tambah component profile dan halaman about us

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-center">
+            <a href="/">
+                <img class="" style="height: 50px" src="/img/logo-ps.png" alt="">
+            </a>
+            <nav class="d-flex align-items-center">
+                <ul class="">
+                    <li><a href="/about">About Us</a></li>
+                    <li class="dot">.</li>
+                    <li><a href="#work" class="r_spacer">Our Products</a></li>
+                    <li class="dot">.</li>
+                    <li><a href="#shop">Our X</a></li>
+                    <li class="dot">.</li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="hero position-relative ">
+        <div class=" text-center text-light position-absolute top-50 start-50 translate-middle">
+            <b class="fs-1">PT Juara Agroniaga Sejahtera</b>
+            <br>
+            Memadukan Sumberdaya Bermental Juara untuk mencapai kesejahteraan bersama
+        </div>
+
+    </div>
+</header>
